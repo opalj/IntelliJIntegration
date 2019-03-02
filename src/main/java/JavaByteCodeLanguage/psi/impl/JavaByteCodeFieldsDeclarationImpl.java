b29318aaa7a301c1cd49a8ce16b671c64139de71
Javor Nikolov <javorxxbence@googlemail.com>
2019-03-02 21:25:38 +0100

    Opal merged

---
+++ src/main/java/JavaByteCodeLanguage/psi/impl/JavaByteCodeFieldsDeclarationImpl.java
@@ -0,0 +1,41 @@
+// This is a generated file. Not intended for manual editing.
+package JavaByteCodeLanguage.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static JavaByteCodeLanguage.psi.JavaByteCodeTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import JavaByteCodeLanguage.psi.*;
+
+public class JavaByteCodeFieldsDeclarationImpl extends ASTWrapperPsiElement implements JavaByteCodeFieldsDeclaration {
+
+  public JavaByteCodeFieldsDeclarationImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull JavaByteCodeVisitor visitor) {
+    visitor.visitFieldsDeclaration(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof JavaByteCodeVisitor) accept((JavaByteCodeVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public List<JavaByteCodeDefMethodName> getDefMethodNameList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, JavaByteCodeDefMethodName.class);
+  }
+
+  @Override
+  @NotNull
+  public List<JavaByteCodeType> getTypeList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, JavaByteCodeType.class);
+  }
+
+}
