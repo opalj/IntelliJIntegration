149fa690b0b074d110ad2a125d1ef66ba3eefae9
Javor Nikolov <javorxxbence@googlemail.com>
2019-03-09 21:55:30 +0100

    #2 OpenClassFile Action: menu items are now displayed at every java,scala,class file and the class file of a java file is also passed if it exists in the project scope
    Added Comments
    
    Opal
    checks if a class file has already been passed if this is the case the required data will be cached to save resources
    
    jarfiles are now opened correct
    
    plugin.xml
    added Opal.ba
    
    build Gradle
    added more memory to the JVM

--- src/main/java/Actions/OpenClassFileAction.java
+++ src/main/java/Actions/OpenClassFileAction.java
@@ -44,61 +44,64 @@ public class OpenClassFileAction extends AnAction {
    * @param editorName the editor name witch will be opened
    */
   public OpenClassFileAction(String editorName) {
-    super();this.editorName = editorName;
+    super();
+    this.editorName = editorName;
   }
+
   @Override
   public void update(AnActionEvent e) {
     final Project project = e.getProject();
     final VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
     // show Action only for java files
     e.getPresentation()
-        .setEnabledAndVisible(
-            project != null
-                && ("java".equals(virtualFile.getExtension())
-                    || "scala".equals(virtualFile.getExtension())
-                    ||"class".equals(virtualFile.getExtension())));
+            .setEnabledAndVisible(
+                    project != null
+                            && ("java".equals(virtualFile.getExtension())
+                            || "scala".equals(virtualFile.getExtension())
+                            || "class".equals(virtualFile.getExtension())));
   }
 
   /**
-   *  performed after a clickevent to open a the specified editor
+   * performed after a clickevent to open a the specified editor
+   *
    * @param event
    */
   @Override
   public void actionPerformed(AnActionEvent event) {
     // Load the file
-      final Project project = event.getProject();
-      // currently selected file in the project view
-      VirtualFile virtualFile = event.getData(CommonDataKeys.VIRTUAL_FILE);
-      VirtualFile classFile = null;
-      if(!virtualFile.getExtension().equals(StdFileTypes.CLASS.getDefaultExtension())) {
-        if (Compiler.make(project, virtualFile)) {
-          classFile = getCorrespondingClassFile(project, virtualFile);
-        }else if(ProjectFileIndex.getInstance(project).isInLibrary(virtualFile)) {
-          String FileName = Opal.getJarFileRootAndFileName(virtualFile.getParent())[1];
-          Collection<VirtualFile> virtualFilesByName = FilenameIndex.getVirtualFilesByName(project, virtualFile.getNameWithoutExtension()+".class", GlobalSearchScope.allScope(project));
-          for(VirtualFile vf : virtualFilesByName){
-            if(vf.getPath().contains(FileName)) {
-              classFile = vf;
-              break;
-            }
+    final Project project = event.getProject();
+    // currently selected file in the project view
+    VirtualFile virtualFile = event.getData(CommonDataKeys.VIRTUAL_FILE);
+    VirtualFile classFile = null;
+    if (!virtualFile.getExtension().equals(StdFileTypes.CLASS.getDefaultExtension())) {
+      if (Compiler.make(project, virtualFile)) {
+        classFile = getCorrespondingClassFile(project, virtualFile);
+      } else if (ProjectFileIndex.getInstance(project).isInLibrary(virtualFile)) {
+        String FileName = Opal.getJarFileRootAndFileName(virtualFile.getParent())[1];
+        Collection<VirtualFile> virtualFilesByName = FilenameIndex.getVirtualFilesByName(project, virtualFile.getNameWithoutExtension() + ".class", GlobalSearchScope.allScope(project));
+        for (VirtualFile vf : virtualFilesByName) {
+          if (vf.getPath().contains(FileName)) {
+            classFile = vf;
+            break;
           }
         }
-      }else{
-        classFile = virtualFile;
       }
-    if(classFile != null) {
+    } else {
+      classFile = virtualFile;
+    }
+    if (classFile != null) {
       FileEditorManager.getInstance(project).openFile(classFile, true);
       FileEditorManager.getInstance(project).setSelectedEditor(classFile, editorName);
-    }else {
+    } else {
       Notifications.Bus.notify(new Notification("OpalPlugin", "OpalPlugin", "can't find or create a class file for : " + virtualFile.getName(), NotificationType.INFORMATION));
     }
-    } // actionPerformed
+  } // actionPerformed
 
   /**
    * Will look for the corresponding .class-file of the passed in .java-file in the output directory
    * of the module, and return it
    *
-   * @param project the current project (needed to determine the current module)
+   * @param project  the current project (needed to determine the current module)
    * @param javaFile the .java-file whose corresponding .class-file we're looking for
    * @return the .class-File corresponding to javaFile
    */
@@ -134,49 +137,7 @@ public class OpenClassFileAction extends AnAction {
       }
       return classFile;
     }
-    //else if (javaFile.getPath().contains("!")) {
-//      File tempJavaFile = null;
-//      File tempClassFile = null;
-//      try {
-//        tempJavaFile = new File(FileUtil.getTempDirectory() + File.separator + javaFile.getName());
-//        Document document = FileDocumentManager.getInstance().getDocument(javaFile);
-//        FileUtil.writeToFile(tempJavaFile, document.getText());
-//        VirtualFile virtualTempJavaFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(tempJavaFile);
-//        //todo should maybe be dont with compileManager(Impl).compile(new VirtualFile[]{virtualTempJavaFile},null);
-//        String jarFilePath = Opal.getJarFileRoot(javaFile)
-//                .substring(0, Opal.getJarFileRoot(javaFile).lastIndexOf("/") + 1);
-//        String reduceequalsfold =
-//                recFolder(new File(jarFilePath), new ArrayList<String>()).stream().reduce("", (s, s2) -> s.concat("/*;").concat(s2)).replaceFirst("/\\*;", "");
-//        RunCommand.runJavaC(tempJavaFile.getPath(), reduceequalsfold);
-//        tempClassFile =
-//                new File(
-//                        FileUtil.getTempDirectory()
-//                                + File.separator
-//                                + javaFile.getNameWithoutExtension()
-//                                + ".class");
-//      } catch (IOException e) {
-//        e.printStackTrace();
-//      }
-//      return LocalFileSystem.getInstance().refreshAndFindFileByIoFile(tempClassFile);
-//    }
     return null;
-  }// getCorrespondingClassFile()
-  /**
-   * helpermethod to find all path that contains jars.
-   *
-   * @param parrent the parrentfolder
-   * @param jarName the jarPath array
-   * @return the jarPath array list
-   */
-  public ArrayList<String> recFolder(File parrent, ArrayList<String> jarName) {
-    for (File file : parrent.listFiles()) {
-      if (file.isDirectory()) {
-        recFolder(file, jarName);
-      }else if(file.getName().contains("jar")){
-        if(!jarName.contains(file.getParentFile() + "\\"))
-        jarName.add(file.getParentFile() + "\\");
-      }
-    }
-    return jarName;
-  }
+  } // getCorrespondingClassFile()
 }
+
