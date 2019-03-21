2b92d91111546622ff4e1a461236144e34794a25
Marcel <mazzel.dieter@gmail.com>
2019-03-21 19:57:32 +0100

    added files to git

---
+++ src/main/java/syntaxHighlighting/TACJMethodHead.java
@@ -0,0 +1,40 @@
+// This is a generated file. Not intended for manual editing.
+package syntaxHighlighting;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface TACJMethodHead extends PsiElement {
+
+  @Nullable
+  TACType getType();
+
+  @Nullable
+  PsiElement getComment();
+
+  @Nullable
+  PsiElement getKeywords();
+
+  @NotNull
+  PsiElement getLBracket();
+
+  @Nullable
+  PsiElement getLDoubleLaceBrace();
+
+  @Nullable
+  PsiElement getLLaceBrace();
+
+  @NotNull
+  PsiElement getRBracket();
+
+  @Nullable
+  PsiElement getRDoubleLaceBrace();
+
+  @Nullable
+  PsiElement getRLaceBrace();
+
+  @Nullable
+  PsiElement getVoid();
+
+}
