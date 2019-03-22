09eb1be93a30c9a24c21212499e56e19a1f8457a
kebiro <bekiroe@hotmail.de>
2019-03-22 11:30:22 +0100

    Added more tables Added grammar Got rid of hotkeys for action as they caused problems (new ones don't work yet) See try/catch with InnerClassTable (sometimes throws NoSuchElementException)

--- src/main/java/opalintegration/JbcProducer.java
+++ src/main/java/opalintegration/JbcProducer.java
@@ -292,7 +292,7 @@ class JbcProducer extends DecompiledTextProducer {
                     localVariableType.startPC() + localVariableType.length(),
                     localVariableType.index(),
                     localVariableType.name(),
-                    localVariableType.signature().toJVMSignature());
+                    localVariableType.signature().toJVMSignature().replaceAll(";>", ">"));
             localVariableTypeTable.append(localVariableTypeEntry);
           }
           return v1.toString();
